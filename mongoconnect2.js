80cfc03e8924c4a066c473cbdb2bb6172a3c9e2b
riya40 <priyanunna20@gmail.com>
2023-05-29 17:17:05 +0530

    Added:wildcard serach and calling the function

--- mongoconnect2.js
+++ mongoconnect2.js
@@ -54,6 +54,63 @@ async function deleteDocument(empid){
         console.error("error in user Deletion",error)
     }
 }
+async function wildCardSearchdisplayEmployee(searchParams){
+    try{
+        const searchCriteria = {
+            $and:[
+                {name:{$regex :new RegExp(searchParams.name,'i')}},
+                {email:{$regex :new RegExp(searchParams.email,'i')}}
+            ]
+        };
+        const employee = await Employee.find(searchCriteria);
+        console.log('employee:')
+        employee.forEach((employee) => {
+            console.log(`name:${employee.empid},name:${employee.name},email:${employee.email}`);
+        });
+    }catch(error){
+        console.error('error searching in employee:',error)
+    }
+}
+
+
+async function dispalyrecords(){
+    try{
+        const employee = await Employee.find()
+        console.log("Employees:") 
+        employee.forEach((employee) => {
+            console.log(`name:${employee.empid},name:${employee.name},email:${employee.email}`);
+        });
+    }catch(error){
+        console.log("error in display records",error)
+    }
+}
+
+async function main(){
+    console.log("-------Connecting to database-------")
+    await connectToDatabase();
+    console.log("-----------creating the user documents------------")
+    await createDocument(108,'tripura','edcba@gmail.com')
+    
+    console.log("-----------creating the updating documents------------")
+    await updateDocument(101,'priya','priyanka@gmail.com')
+
+    await wildCardSearchdisplayEmployee({name:'a',email:'gmail.com'})
+    console.log("---------displaying the records----------")
+    await dispalyrecords()
+    console.log("-----------Deleting the user documents------------")
+    await deleteDocument(101);
+    await deleteDocument(102);
+    await deleteDocument(103);
+    await deleteDocument(104);
+    
+    
+    console.log("------end------")
+    mongoose.disconnect()
+}
+
+main()
+
+
 
 
 
